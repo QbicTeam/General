18bb731a859738eead84209983ef6bb9deb8f0bc
Majahide Payan <majahide.payan@hotmail.com>
2020-05-07 21:23:15 -0700

    Starting Seed Proyect Solution

---
+++ Framework/Framework.DataTypes/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Framework.DataTypes
+{
+    public class Class1
+    {
+    }
+}
---
+++ Framework/Framework.Helpers/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Framework.Helpers
+{
+    public class Class1
+    {
+    }
+}
