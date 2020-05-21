100c6f35e47c5827bc16e0626d57af636888bdac
Majahide Payan <majahide.payan@hotmail.com>
2020-05-20 20:45:37 -0700

    Mocking starting application

--- SIQbic/SIQbic-SPA/src/app/app-routing.module.ts
+++ SIQbic/SIQbic-SPA/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+      path: '',
+      children: [
+          { path: 'home', component: HomeComponent },
+          { path: 'login', component: LoginComponent },
+          { path: 'register', component: RegisterComponent}
+      ]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full'}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SIQbic/SIQbic-SPA/src/app/app.module.ts
+++ SIQbic/SIQbic-SPA/src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MaindashboardComponent } from './maindashboard/maindashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavbarComponent,
+      SidebarComponent,
+      FooterComponent,
+      MaindashboardComponent,
+      HomeComponent,
+      LoginComponent,
+      RegisterComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
